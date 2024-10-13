673dab2897910dce63f5012fcebcb57a241a7874
saqlainhaider786 <saqlainhaider786985@gmail.com>
2024-10-13 19:01:58 +0500

    Node & Express js tutorials

---
+++ Rest_API/index.js
@@ -0,0 +1,9 @@
+/*
+==================== Rest API ====================
+-> Rest API is a type of API which have some rules : 
+i) It works on server client architecture and server & client should not 
+depend on each other.(server's response type should not effect client side).
+
+ii) Always respect all http methods means use every method to perform only
+those tasks which they suite.
+*/
